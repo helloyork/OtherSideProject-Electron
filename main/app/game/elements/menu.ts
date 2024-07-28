4939e354c1731b1f9ea1883e690d8a4870a9afbc
helloyork <helloyork@icloud.com>
2024-07-28 16:13:33 -0700

    修改界面

---
+++ main/app/game/elements/menu.ts
@@ -0,0 +1,26 @@
+import { Constructable } from "../constructable";
+import { Game, LogicNode } from "../game";
+import { deepMerge } from "../../../util/data";
+
+export type StoryConfig = {};
+
+const { StoryAction } = LogicNode;
+export class Story extends Constructable<
+    any,
+    LogicNode.SceneAction<"scene:action">,
+    LogicNode.StoryAction<"story:action">
+> {
+    static defaultConfig: StoryConfig = {};
+    static targetAction = StoryAction;
+    id: string;
+    name: string;
+    config: StoryConfig;
+
+    constructor(name: string, config: StoryConfig = {}) {
+        super();
+        this.id = Game.getIdManager().getStringId();
+        this.name = name;
+        this.config = deepMerge<StoryConfig>(Story.defaultConfig, config);
+    }
+}
+
